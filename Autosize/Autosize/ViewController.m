56938ccaef54c0d2f20282aeb9444e322081e625
smallg <gwpxjb@163.com>
2015-07-29 15:17:17 +0800

    add autosize project

---
+++ Autosize/Autosize/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Autosize
+//
+//  Created by User3 on 7/17/15.
+//  Copyright (c) 2015 User3. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
