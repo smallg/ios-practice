2ea3ac48eaa9db66e35b26b912ded06357b0328e
smallg <gwpxjb@163.com>
2015-07-31 09:55:38 +0800

    custom table cell step nib

---
+++ Sections/Sections/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sections
+//
+//  Created by User3 on 7/31/15.
+//  Copyright (c) 2015 User3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
