96e555e477a98effa841fbe95c564578fb80788b
smallg <gwpxjb@163.com>
2015-08-07 10:27:43 +0800

    Google GCM test

---
+++ Google GCM Test/Google GCM Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Google GCM Test
+//
+//  Created by User3 on 8/7/15.
+//  Copyright (c) 2015 Smallg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Google GCM Test/Google GCM Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Google GCM Test
+//
+//  Created by User3 on 8/7/15.
+//  Copyright (c) 2015 Smallg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
