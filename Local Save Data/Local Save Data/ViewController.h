50afc2568fe1003881f52c8f40172a8790727486
smallg <gwpxjb@163.com>
2015-08-11 10:44:05 +0800

    local save data

---
+++ Local Save Data/Local Save Data/ViewController.h
@@ -0,0 +1,21 @@
+//
+//  ViewController.h
+//  Local Save Data
+//
+//  Created by User3 on 8/7/15.
+//  Copyright (c) 2015 Smallg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *txtInteger;
+    IBOutlet UILabel *txtFloat;
+    IBOutlet UILabel *txtDouble;
+    IBOutlet UILabel *txtNSString;
+    IBOutlet UILabel *txtNSDate;
+    IBOutlet UILabel *txtNSArray;
+    IBOutlet UILabel *txtNSDictionary;
+}
+@end
+
