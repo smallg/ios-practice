aae63fc6ba32e6d05677bb73af85cc3a291dbdae
smallg <gwpxjb@163.com>
2015-08-18 17:12:51 +0800

    section 12, step 1

---
+++ Bridge Control/Bridge Control/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Bridge Control
+//
+//  Created by User3 on 8/18/15.
+//  Copyright (c) 2015 Smallg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
