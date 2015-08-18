1e75d80a111089763a75d8874c051996e914469d
smallg <gwpxjb@163.com>
2015-08-18 16:37:24 +0800

    section 10 end

--- DialogViewer/DialogViewer/ViewController.m
+++ DialogViewer/DialogViewer/ViewController.m
@@ -21,25 +21,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.sections = @[
-                      @{@"header" : @"First",
-                        @"content" : @"Hey, Small1"},
-                      @{@"header" : @"Second",
-                        @"content" : @"Hey, Small2"},
-                      @{@"header" : @"Third",
-                        @"content" : @"Hey, Small3"},
-                      @{@"header" : @"Forth",
-                        @"content" : @"Hey, Small4"},
-                      @{@"header" : @"Fifth",
-                        @"content" : @"Hey, Small5"},
-                      @{@"header" : @"Sixth",
-                        @"content" : @"Hey, Small6"}];
+    self.sections =
+    @[
+      @{ @"header" : @"First Witch",
+         @"content" : @"Hey, when will the three of us meet up later?" },
+      @{ @"header" : @"Second Witch",
+         @"content" : @"When everything's straightened out." },
+      @{ @"header" : @"Third Witch",
+         @"content" : @"That'll be just before sunset." },
+      @{ @"header" : @"First Witch",
+         @"content" : @"Where?" },
+      @{ @"header" : @"Second Witch",
+         @"content" : @"The dirt patch." },
+      @{ @"header" : @"Third Witch",
+         @"content" : @"I guess we'll see Mac there." },
+      ];
     
     [self.collectionView registerClass:[ContentCell class] forCellWithReuseIdentifier:@"CONTENT"];
     self.collectionView.backgroundColor = [UIColor whiteColor];
     UIEdgeInsets contentInset = self.collectionView.contentInset;
     contentInset.top = 20;
     [self.collectionView setContentInset:contentInset];
+    
+    // flow config
+    UICollectionViewLayout *layout = self.collectionView.collectionViewLayout;
+    UICollectionViewFlowLayout *flow = (UICollectionViewFlowLayout *)layout;
+    flow.sectionInset = UIEdgeInsetsMake(10, 20, 30, 20);
+    
+    [self.collectionView registerClass:[HeaderCell class]
+            forSupplementaryViewOfKind:UICollectionElementKindSectionHeader
+                   withReuseIdentifier:@"HEADER"];
+    
+    flow.headerReferenceSize = CGSizeMake(100, 25);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -52,6 +65,47 @@
     NSCharacterSet *space = [NSCharacterSet whitespaceAndNewlineCharacterSet];
     NSArray *words = [content componentsSeparatedByCharactersInSet:space];
     return words;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return [self.sections count];
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    NSArray *words = [self wordsInSection:section];
+    return [words count];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    NSArray *words = [self wordsInSection:indexPath.section];
+    ContentCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"CONTENT" forIndexPath:indexPath];
+    cell.text = words[indexPath.row];
+    return cell;
+}
+
+#pragma mark - Flow Layout Delegate
+
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout*)collectionViewLayout
+  sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSArray *words = [self wordsInSection:indexPath.section];
+    CGSize size = [ContentCell sizeForContentString:words[indexPath.row]];
+    return size;
+}
+
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView
+           viewForSupplementaryElementOfKind:(NSString *)kind
+                                 atIndexPath:(NSIndexPath *)indexPath {
+    if ([kind isEqual:UICollectionElementKindSectionHeader]) {
+        HeaderCell *cell = [self.collectionView
+                               dequeueReusableSupplementaryViewOfKind:kind
+                               withReuseIdentifier:@"HEADER"
+                               forIndexPath:indexPath];
+        
+        cell.text = self.sections[indexPath.section][@"header"];
+        return cell;
+    }
+    return nil;
 }
 
 @end
